3a6204fa0380b5d30923ec63b63131cae3ebc1de
Khilraj shrestha <khilraj321@gmail.com>
2023-02-26 15:13:59 +0545

    question added

--- src/Question1B.java
+++ src/Question1B.java
@@ -1,4 +1,86 @@
 package src;
 
+import java.util.ArrayList;
+
+import static src.Question1B.Graph.*;
+
 public class Question1B {
+
+    public static void main(String[] args) {
+
+        int V = 8;
+        ArrayList<ArrayList<Integer>> am = new ArrayList<ArrayList<Integer>>(V);
+
+        for (int i = 0; i < V; i++)
+            am.add(new ArrayList<Integer>());
+        addEdge(am, 0, 1);
+        addEdge(am, 0, 2);
+        addEdge(am, 1, 3);
+        addEdge(am, 2, 4);
+        addEdge(am, 1, 6);
+        addEdge(am, 4, 6);
+        addEdge(am, 4, 5);
+        addEdge(am, 5, 7);
+        int disconnectedNode = 4;
+        printDisconnectedNodes(am, disconnectedNode);
+
+        int nodes[] = {0, 1, 2, 3, 4, 5, 6, 7};
+        int destination = 0;
+        for (int i = 0; i < nodes.length; i++) {
+            int source = nodes[i];
+            boolean disconn = isReachable(am, source, destination);
+            if (disconn == false) {
+                System.out.println("disconnected nodes are " + nodes[i]);
+            }
+
+        }
+    }
+
+    class Graph {
+        static void addEdge(ArrayList<ArrayList<Integer>> am, int s, int d) {
+            am.get(s).add(d);
+            am.get(d).add(s);
+        }
+
+
+        static void removeEdge(ArrayList<ArrayList<Integer>> am, int s, int d) {
+            if (am.get(s).contains(d)) {
+                am.get(s).remove(Integer.valueOf(d));
+                System.out.println("removed node " + s);
+            }
+            if (am.get(d).contains(s)) {
+                am.get(d).remove(Integer.valueOf(s));
+                System.out.println("removed node " + d);
+            }
+        }
+
+
+        static void printDisconnectedNodes(ArrayList<ArrayList<Integer>> am, int disconnectedNode) {
+            for (int i = am.get(disconnectedNode).size() - 1; i >= 0; i--) {
+                int neighbor = am.get(disconnectedNode).get(i);
+                System.out.println("Removing edge between " + disconnectedNode + " and " + neighbor);
+                removeEdge(am, disconnectedNode, neighbor);
+            }
+        }
+
+        static boolean isReachable(ArrayList<ArrayList<Integer>> am, int s, int d) {
+            boolean[] visited = new boolean[am.size()];
+            return dfs(am, visited, s, d);
+        }
+
+        static boolean dfs(ArrayList<ArrayList<Integer>> am, boolean[] visited, int s, int d) {
+            visited[s] = true;
+            if (s == d) {
+                return true;
+            }
+            for (int i = 0; i < am.get(s).size(); i++) {
+                int v = am.get(s).get(i);
+                if (!visited[v] && dfs(am, visited, v, d)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
--- src/Question7A.java
+++ src/Question7A.java
@@ -1,84 +1,78 @@
 package src;
 
+import java.util.Arrays;
 
 public class Question7A {
+    public class MatrixMultiplication {
 
-    public static void main(String[] args) {
-        MatrixMultiplication matrixMultiplication = new MatrixMultiplication();
-        matrixMultiplication.runMatrixMultiplication();
-    }
+        public static int[][] multiply(int[][] A, int[][] B, int numThreads) throws InterruptedException {
+            int n = A.length;
+            int[][] C = new int[n][n];
 
-    public static class MatrixMultiplication {
-
-        private static int[][] x;
-        private static int[][] y;
-        private static int[][] z;
-        private static int n;
-        private static int numThreads;
-
-        public void runMatrixMultiplication() {
-            n = 1000;
-            numThreads = 4;
-            x = new int[n][n];
-            y = new int[n][n];
-            z = new int[n][n];
-
-            // Initialize matrices A and B with random values
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    x[i][j] = (int) (Math.random() * 100);
-                    y[i][j] = (int) (Math.random() * 100);
-                }
-            }
-
-            // Create threads to multiply slices of the matrices
+            // create an array of threads
             Thread[] threads = new Thread[numThreads];
-            int sliceSize = n / numThreads;
-            for (int i = 0; i < numThreads; i++) {
-                int start = i * sliceSize;
-                int end = (i + 1) * sliceSize;
-                threads[i] = new Thread(new MatrixMultiplier(start, end));
-                threads[i].start();
+
+            // calculate number of rows each thread should handle
+            int rowsPerThread = n / numThreads;
+
+            for (int t = 0; t < numThreads; t++) {
+                // calculate start and end rows for this thread
+                int startRow = t * rowsPerThread;
+                int endRow = (t == numThreads-1) ? n : (t+1) * rowsPerThread;
+
+                // create a new thread to handle the assigned rows
+                threads[t] = new Thread(new MatrixMultiplicationThread(A, B, C, startRow, endRow));
+                threads[t].start();
             }
 
-            // Wait for all threads to complete
-            for (int i = 0; i < numThreads; i++) {
-                try {
-                    threads[i].join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            // wait for all threads to finish
+            for (Thread thread : threads) {
+                thread.join();
             }
 
-            // Print the result
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(z[i][j] + " ");
-                }
-                System.out.println();
-            }
+            return C;
         }
 
-        private static class MatrixMultiplier implements Runnable {
+        private static class MatrixMultiplicationThread implements Runnable {
+            private int[][] A;
+            private int[][] B;
+            private int[][] C;
+            private int startRow;
+            private int endRow;
 
-            private int start;
-            private int end;
-
-            public MatrixMultiplier(int start, int end) {
-                this.start = start;
-                this.end = end;
+            public MatrixMultiplicationThread(int[][] A, int[][] B, int[][] C, int startRow, int endRow) {
+                this.A = A;
+                this.B = B;
+                this.C = C;
+                this.startRow = startRow;
+                this.endRow = endRow;
             }
 
-            @Override
             public void run() {
-                for (int i = start; i < end; i++) {
+                int n = A.length;
+                for (int i = startRow; i < endRow; i++) {
                     for (int j = 0; j < n; j++) {
                         for (int k = 0; k < n; k++) {
-                            z[i][j] += x[i][k] * y[k][j];
+                            C[i][j] += A[i][k] * B[k][j];
                         }
                     }
                 }
             }
         }
     }
+    public static void main(String[] args) {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{5, 6}, {7, 8}};
+
+        try {
+            int[][] C = MatrixMultiplication.multiply(A, B, 2);
+            // print the resulting matrix C
+            for (int[] row : C) {
+                System.out.println(Arrays.toString(row));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
